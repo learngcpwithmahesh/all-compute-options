5b4b10989eb8eb0d33c1b231643aedc81547d6f1
Learn GCP with Mahesh <learngcpwithmahesh@gmail.com>
2022-12-31 18:04:53 +0000

    Happy New Year

---
+++ gce/startup-script.sh
@@ -0,0 +1,4 @@
+apt update 
+apt install git nginx -y
+cd /var/www/html
+git clone https://github.com/learngcpwithmahesh/all-compute-options.git
---
+++ gcs/script.sh
@@ -0,0 +1,2 @@
+gcloud storage buckets create gs://happy-new-year-2023
+gcloud storage cp index.html gs://happy-new-year-2023
